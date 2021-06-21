d02a55f582e6087e45cf3d8bf933571c2f57c7bf
GrakePCH <1152932438@qq.com>
2021-06-21 00:46:18 -0700

    Rebuild Appbar with Material-UI components

--- client-side/src/components/Dashboard/Dashboard.jsx
+++ client-side/src/components/Dashboard/Dashboard.jsx
@@ -1,214 +1,297 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import clsx from 'clsx';
 import useStyles from './styles';
-import {ListItems} from './listItems';
-import {Typography, AppBar, Button,  CssBaseline, Toolbar} from '@material-ui/core';
-import {useDispatch} from 'react-redux';
+import { ListItems } from './listItems';
+
+/*Material-UI Components*/
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import deepPurple from '@material-ui/core/colors/deepPurple';
+import {
+  Typography,
+  AppBar,
+  Avatar,
+  Button,
+  CssBaseline,
+  Toolbar,
+  Drawer,
+  List,
+  Divider,
+  IconButton,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  InputBase
+} from '@material-ui/core';
+
+/*Material-UI Icons*/
 import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 import ClosePageIcon from '@material-ui/icons/Close';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import CreateIcon from '@material-ui/icons/Create';
+import AddIcon from '@material-ui/icons/Add';
+
 import GenresLogo from '../Assets/logo-svg2.svg';
-import {Link, useHistory, useLocation} from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import decode from 'jwt-decode';
 import styled from 'styled-components';
 import Popup from '../Auth/Popup';
 import Auth from '../Auth/Auth';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import CreateIcon from '@material-ui/icons/Create';
-import Searchbar from '../Searchbar/Searchbar'
+// import Searchbar from '../Searchbar/Searchbar'
 
+/* */
+// const SidebarLink = styled(Link)`
+//   display: flex;
+//   color: black;
+//   justify-content: left;
+//   align-items: center;
+//   padding: 15px;
+//   list-style: none;
+//   height: 55px;
+//   text-decoration: none;
+//   font-size: 16px;
+//   &:hover {
+//     background: lavender;
+//     border-left: 4px solid #55fccf;
+//     cursor: pointer;
+//   }
+// `;
 
-const SidebarLink = styled(Link)`
-  display: flex;
-  color: black;
-  justify-content: left;
-  align-items: center;
-  padding: 15px;
-  list-style: none;
-  height: 55px;
-  text-decoration: none;
-  font-size: 16px;
-  &:hover {
-    background: lavender;
-    border-left: 4px solid #55fccf;
-    cursor: pointer;
-  }
-`;
+// const SidebarLabel = styled.span`
+//   margin-left: 40px;
+// `;
 
-const SidebarLabel = styled.span`
-  margin-left: 40px;
-`;
+// const SidebarIcon = styled.span`
+//   margin-top: 3px;
+//   margin-left: 10.5px;
+// `;
 
-const SidebarIcon = styled.span`
-  margin-top: 3px;
-  margin-left: 10.5px;
-`;
+// const NavIcon = styled(Link)`
+//   color: black;
+//   font-size: 2rem;
+//   margin-right: 15px;
+//   height: 80px;
+//   display: flex;
+//   justify-content: flex-start;
+//   align-items: center;
+// `;
 
-const NavIcon = styled(Link)`
-  color: black;
-  font-size: 2rem;
-  margin-right: 15px;
-  height: 80px;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-`;
+// const CloseIcon = styled(Link)`
+//   color: black;
+//   margin-left: 1.6rem;
+//   font-size: 2rem;
+//   height: 80px;
+//   display: flex;
+//   justify-content: flex-start;
+//   align-items: center;
+// `;
 
-const CloseIcon = styled(Link)`
-  color: black;
-  margin-left: 1.6rem;
-  font-size: 2rem;
-  height: 80px;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-`;
+// const SidebarNav = styled.nav`
+//   background: white;
+//   width: 240px;
+//   height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   position: fixed;
+//   top: 0;
+//   left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
+//   transition: 250ms;
+//   z-index: 10;
+//   filter: drop-shadow(0 0 5px #9999);
+// `;
 
-const SidebarNav = styled.nav`
-  background: white;
-  width: 240px;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  position: fixed;
-  top: 0;
-  left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
-  transition: 250ms;
-  z-index: 10;
-  filter: drop-shadow(0 0 5px #9999);
-`;
-
-const SidebarWrap = styled.div`
-  width: 100%;
-`;
+// const SidebarWrap = styled.div`
+//   width: 100%;
+// `;
 
 const Dashboard = () => {
-    const classes = useStyles();
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
-    const [openPopup, setOpenPopup] = useState(false);
-    const [sidebar, setSidebar] = useState(false);
-    const showSidebar = () => setSidebar(!sidebar);    
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const location = useLocation();
-    const genres = ["Action","Fantasy","Science-Fiction","Romance", "Mystery", "Horror", "Thriller", "Fiction", "Dystopian"];
+  const classes = useStyles();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const [openPopup, setOpenPopup] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+  const genres = ["Action", "Fantasy", "Science-Fiction", "Romance", "Mystery", "Horror", "Thriller", "Fiction", "Dystopian"];
 
-    const logout = () => {
-      dispatch({type: 'LOGOUT'});
-      history.push('/');
+  const logout = () => {
+    handleDrawerClose();
+    dispatch({ type: 'LOGOUT' });
+    history.push('/');
 
-      setUser(null);
-    };
+    setUser(null);
+  };
 
-    const switchPopup = () => {
-      setOpenPopup((openPopup) => !openPopup);
-    };
+  const switchPopup = () => {
+    setOpenPopup((openPopup) => !openPopup);
+  };
 
-    useEffect(() =>{
-      const token = user?.token;
-      //JWT
-      
-      if(token) {
-        const decodedToken = decode(token);
-        if (decodedToken.exp*1000 < new Date().getTime()) logout();
-      }
+  useEffect(() => {
+    const token = user?.token;
+    //JWT
+
+    if (token) {
+      const decodedToken = decode(token);
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
 
 
-      setUser(JSON.parse(localStorage.getItem('profile')));
-      setOpenPopup(false);
-    }, [location]);
-  
+    setUser(JSON.parse(localStorage.getItem('profile')));
+    setOpenPopup(false);
+  }, [location]);
+
+  document.body.style.margin = "0";
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#ffffff',
+      },
+      secondary: deepPurple,
+      alert: '#ff1744',
+    },
+  });
+
   return (
-    <div className={classes.root}>
+    <ThemeProvider theme={theme}>
+    <div className = {classes.root}>
       <CssBaseline />
-      <AppBar position="relative" style = {{background: 'lavender'}}elevation = {0}>
-        <Toolbar className={classes.toolbar}>
-        {
-                user ? (
-                    <>
-                    <NavIcon>
-              <MenuIcon onClick = {showSidebar}/>
-          </NavIcon>
-          
-
-          <SidebarNav sidebar = {sidebar}>
-            <SidebarWrap>
-            <NavIcon>
-            <MenuIcon style={{marginLeft: "25px"}} onClick = {showSidebar}/>
-            </NavIcon>     
-            <Link to= "/profile">
-            <AccountCircle style={{fontSize : "100px", marginLeft: "65px"}}  className= {classes.accountCircle}/>
-            </Link>
-                        
-                        
-                        
-
-            {ListItems.map((item, index) => {
-              return (
-                <SidebarLink to={item.path} key = {index}>
-                  {/* <div> */}
-                  <SidebarIcon>{item.icon}</SidebarIcon>
-                  <SidebarLabel>{item.title}</SidebarLabel>
-                  {/* </div> */}
-                </SidebarLink>
-              );
-            })}
-            
-            <Button style={{marginBottom: "30px" ,marginLeft: "70px", marginTop: "600px"}} variant = "contained" className={classes.buttonOutline} onClick ={logout}>Logout</Button>
-            
-          </SidebarWrap>
-        </SidebarNav>
-                    </>
-                ) : (
-                    <></>
-                )
+      <AppBar
+        position = "fixed"
+        className = {clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          {
+            user ? (
+              <>
+                <IconButton
+                  color = "inherit"
+                  aria-label = "open drawer"
+                  onClick = {handleDrawerOpen}
+                  edge = "start"
+                  className = {clsx(classes.menuButton, open && classes.hide)}
+                >
+                  <MenuIcon />
+                </IconButton>
+              </>
+            ) : (
+              <></>
+            )
           }
 
-          
+          {/* <Typography variant = "h6" className = {classes.title}>
+            Genres
+          </Typography> */}
           <Link to = "/">
-          <img src = {GenresLogo} height = '50px' alt = ""></img>
+            <img src = {GenresLogo} height = '50px' alt = ""></img>
           </Link>
 
-          
-            
-          <Typography variant="h6" className={classes.title}></Typography>  
-          
+          <div className = {classes.grow} />
+
           {
-                user ? (
-                    <>
-                    <Searchbar items = {genres}/>
-                    </>
-                ) : (
-                    <Typography></Typography>
-                )
+            user ? (
+              <>
+                <div className = {classes.search}>
+                  <div className = {classes.searchIcon}>
+                    <SearchIcon />
+                  </div>
+                  <InputBase
+                    placeholder = "Search…"
+                    classes = {{
+                      root: classes.inputRoot,
+                      input: classes.inputInput,
+                    }}
+                    inputProps = {{ 'aria-label': 'search' }}
+                  />
+                </div>
+              </>
+            ) : (
+              <Typography></Typography>
+            )
           }
-            
-            <Typography variant="h6" className={classes.title}>
-            </Typography>
-            {
-                user ? (
-                  /* <Avatar className = {classes.profile} alt ={user.result.username} src = {user.result.imageUrl}>{user.result.username.charAt(0)}</Avatar>
-                   <Button variant = "contained" className={classes.button} onClick ={logout}>Logout</Button> */
-                    <>
-                        <Link to="/writing">
-                        <CreateIcon fontSize ="large" className = {classes.accountCircle}/>
-                        </Link>
-                        <Link to= "/profile">
-                        <AccountCircle fontSize = "large" className= {classes.accountCircle}/>
-                        </Link>
-                        <Button variant = "contained" className={classes.buttonOutline} onClick ={logout}>Logout</Button>
-                        
-                    </>
 
+          <div className = {classes.grow} />
 
-                ) : (
-                    <Button onClick = {() => setOpenPopup(true)} className = {classes.buttonOutline} >Sign in</Button>
-                )
-            }
-            <Popup openPopup = {openPopup}  setOpenPopup = {setOpenPopup}>
-              <Auth  value = {openPopup} onChange = {switchPopup} />
-            </Popup>
+          {
+            user ? (
+              <>
+                <Button
+                  href = "/writing"
+                  variant = "contain"
+                  color = "inherit"
+                  endIcon = {<AddIcon />}
+                  className = {classes.button}
+                >
+                  Create
+                </Button>
+              </>
+            ) : (
+              <Button onClick = {() => setOpenPopup(true)} color = "inherit">Login</Button>
+            )
+          }
+
+          <Popup openPopup = {openPopup} setOpenPopup = {setOpenPopup}>
+            <Auth value = {openPopup} onChange = {switchPopup} />
+          </Popup>
         </Toolbar>
       </AppBar>
+
+      <Drawer
+        variant = "temporary"
+        anchor = "left"
+        open = {open} onClose = {handleDrawerClose}
+        className = {classes.drawer}
+        classes = {{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div
+          className = {clsx(classes.list, {
+            [classes.fullList]: false,
+          })}
+          role = "presentation"
+          onClick = {handleDrawerClose}
+          onKeyDown = {handleDrawerClose}
+        ></div>
+        <Avatar alt = "Remy Sharp" src = "/static/media/corgi.bcb4248a.jpg" className = {classes.avatarOfDrawer} />
+          <Typography className = {classes.userName} variant = "subtitle1">User Name</Typography>
+          <Typography className = {classes.userEmail} variant = "subtitle2">example@email.com</Typography>
+        <div className = {classes.profileBtnsOfDrawer}>
+          <Button className = {classes.profileBtnOfDrawer} variant="contained" color = "secondary" disableElevation>Profile</Button>
+          <Button className = {classes.signOutBtnOfDrawer} onClick ={logout} variant="outlined">Sign out</Button>
+        </div>
+        <Divider />
+        <List>
+          {ListItems.map((item, index) => (
+          <Link to = {item.path}>
+            <ListItem button key = {index}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary = {item.title}/>
+            </ListItem>
+            </Link>
+          ))}
+        </List>
+      </Drawer>
     </div>
+    </ThemeProvider>
   );
 }
 
--- client-side/src/components/Dashboard/styles.js
+++ client-side/src/components/Dashboard/styles.js
@@ -1,80 +1,120 @@
-import {fade, makeStyles} from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 
-const drawerWidth = 240;
+
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
+
+  root: {
+    display: 'flex',
+    flexGrow: 1,
+  },
+
+  grow: {
+    flexGrow: 1,
+  },
+
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+
+  toolbarIcon: {
+    display: 'flex',
+
+    ...theme.mixins.toolbar,
+  },
+
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+
+
+  drawer: {
+    width: 250,
+  },
+
+  drawerPaper: {
+    width: 250,
+  },
+
+  avatarOfDrawer: {
+    width: theme.spacing(8),
+    height: theme.spacing(8),
+    margin: theme.spacing(2),
+  },
+
+  userName: {
+    marginLeft: theme.spacing(2),
+  },
+  userEmail: {
+    marginLeft: theme.spacing(2),
+  },
+
+  profileBtnsOfDrawer: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+  },
+
+  profileBtnOfDrawer: {
+    flex: 1,
+    margin: theme.spacing(1),
+  },
+
+  signOutBtnOfDrawer: {
+    flex: 1,
+    margin: theme.spacing(1),
+    color: 'rgb(255, 23, 68)',
+    border: '1px solid rgba(255, 23, 68, 0.5)',
+
+    "&:hover": {
+      backgroundColor: 'rgb(255, 23, 68, 0.04)',
+    }
+  },
+
+
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+
+  menuButtonHidden: {
+    display: 'none',
+  },
+
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
     },
-    toolbar: {
-      paddingRight: 24, // keep right padding when drawer closed
-    },
-    toolbarIcon: {
-      display: 'flex',
-      
-      ...theme.mixins.toolbar,
-    },
-    appBar: {
-      zIndex: theme.zIndex.drawer + 1,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    },
-    appBarShift: {
-      marginLeft: drawerWidth,
-      width: `calc(100% - ${drawerWidth}px)`,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    menuButton: {
-      marginRight: 36,
-    },
-    menuButtonHidden: {
-      display: 'none',
-    },
-    title: {
-      flexGrow: 1,
-    },
-    drawerPaper: {
-      position: 'relative',
-      whiteSpace: 'nowrap',
-      width: drawerWidth,
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    drawerPaperClose: {
-      overflowX: 'hidden',
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      width: theme.spacing(7),
-      [theme.breakpoints.up('sm')]: {
-        width: theme.spacing(9),
-      },
-    },
-    
-    container: {
-      paddingTop: theme.spacing(4),
-      paddingBottom: theme.spacing(4),
-    },
-    paper: {
-      padding: theme.spacing(2),
-      display: 'flex',
-      overflow: 'auto',
-      flexDirection: 'column',
-    },
-    fixedHeight: {
-      height: 240,
-    },
-    search: {
+  },
+
+
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+
+  fixedHeight: {
+    height: 240,
+  },
+
+  
+  search: {
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.black, 0.15),
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
     marginRight: theme.spacing(2),
     marginLeft: 0,
     width: '100%',
@@ -83,6 +123,7 @@ const useStyles = makeStyles((theme) => ({
       width: 'auto',
     },
   },
+
   searchIcon: {
     padding: theme.spacing(0, 2),
     height: '100%',
@@ -92,9 +133,11 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   inputRoot: {
     color: 'inherit',
   },
+
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
@@ -105,13 +148,20 @@ const useStyles = makeStyles((theme) => ({
       width: '20ch',
     },
   },
+
   profile: {
     marginRight: '12px',
   },
+
   accountCircle: {
-    margin:'5px',
-    color:'gray',
+    margin: '5px',
+    color: 'gray',
   },
+
+  button: {
+    margin: theme.spacing(1),
+  },
+
   buttonOutline: {
     width: '100px',
     height: '36px',
@@ -127,37 +177,39 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: '2px 1000px 1px lavender inset',
     borderRadius: '45px',
     cursor: 'pointer',
-    "&:hover":{
+    "&:hover": {
       boxShadow: 'none',
       color: 'white',
+    }
+  },
+
+  navItem: {
+    width: 'calc(60px * 0.8)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+
+  iconButton: {
+    width: '30px',
+    height: '30px',
+    backgroundColor: 'red',
+    borderRadius: '50%',
+    padding: '5px',
+    margin: '2px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    transition: 'filter 300ms',
+
+    "&:hover": {
+      filter: 'brightness(1.2)',
+    }
   }
-},
-navItem : {
-  width: 'calc(60px * 0.8)',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center', 
-
-},
-iconButton: {
-  width: '30px',
-  height: '30px',
-  backgroundColor: 'red',
-  borderRadius: '50%',
-  padding: '5px',
-  margin: '2px',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  transition: 'filter 300ms',
-
-"&:hover": {
-  filter:'brightness(1.2)',
-}
-}
 
 
 
-  }));
+}));
 
-  export default useStyles;
+export default useStyles;
