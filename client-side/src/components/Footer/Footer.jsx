676e44042d1fb14f3fdaa472d0a7b508a10899d7
GrakePCH <1152932438@qq.com>
2021-08-05 02:31:55 -0700

    Fix Footer

--- client-side/src/components/Footer/Footer.jsx
+++ client-side/src/components/Footer/Footer.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
-import {Typography } from '@material-ui/core';
+import { Typography, Link } from '@material-ui/core';
 import useStyles from './styles';
 
 const Footer = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
 
-    return (
-        <>
-        <footer className = {classes.footer}>
-                <Typography variant = "h6" align ="center" gutterBottom>
-                    
-                </Typography>
-                <Typography variant = "subtitle1" align = "center" color = "textSecondary">
-                
-                </Typography>
-        </footer>
-        </>
-    )
+  return (
+    <>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Genres
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          <Link href="#" color="inherit">
+            Terms
+          </Link>
+        </Typography>
+      </footer>
+    </>
+  )
 }
 
 export default Footer
--- client-side/src/components/Footer/styles.js
+++ client-side/src/components/Footer/styles.js
@@ -5,6 +5,7 @@ const useStyles = makeStyles((theme) => ({
     footer:{
         backgroundColor: theme.palette.background.paper,
         padding: '50px 0',
+        marginTop: 64,
     }
 
   }));
