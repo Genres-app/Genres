c2d3d7a430291fd2795861dcbae09c64f8c890f0
GrakePCH <1152932438@qq.com>
2021-09-05 14:50:51 -0700

    BetaReading subpage initial implementation

--- client-side/src/components/BetaReadingBody/BetaReadingGeneral.jsx
+++ client-side/src/components/BetaReadingBody/BetaReadingGeneral.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 
-import { Container, Accordion, AccordionSummary, AccordionDetails, Typography } from '@material-ui/core';
+import { Container, Grid, Accordion, AccordionSummary, AccordionDetails, Typography } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import { BookLib } from '../BookLib';
@@ -10,7 +10,30 @@ import { generalList } from './BetaReadingGeneralList';
 
 
 const useStyles = makeStyles((theme) => ({
-
+  bg: {
+    position: 'absolute',
+    backgroundColor: '#855cde',
+    height: 320,
+    width: '100%',
+    zIndex: -1,
+  },
+  root: {
+    display: "block",
+    minWidth: 250,
+    width: '75%',
+    margin: '0 auto',
+  },
+  titleContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    height: 250,
+    padding: 0,
+    color: theme.palette.background.paper,
+  },
+  title: {
+    paddingBottom: theme.spacing(1),
+    fontWeight: 'bold',
+  }
 }));
 
 const BetaReadingGeneral = ({ theme }) => {
@@ -18,18 +41,28 @@ const BetaReadingGeneral = ({ theme }) => {
   console.log("general");
   return (
     <>
-    <Typography>General Advising</Typography>
-      {generalList.map((item, index) => (
-        <Accordion>
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{BookLib[item].title}</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>{BookLib[item].info}</Typography>
-          </AccordionDetails>
-        </Accordion>
-      ))
-      }
+      <div className={classes.bg}></div>
+      <Container className={classes.root}>
+        <Container className={classes.titleContainer}>
+          <div>
+            <Typography variant='h5'>Beta-Reading /</Typography>
+            <Typography variant='h3' className={classes.title}>General Advising</Typography>
+            <Typography variant='body1'>Works in general advising are looking for feedback on the overall chapter or story. Authors would like to hear the experiences of readers.</Typography>
+          </div>
+        </Container>
+        {
+          generalList.map((item, index) => (
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>{BookLib[item].title}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>{BookLib[item].info}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          ))
+        }
+      </Container>
     </>
   )
 }
