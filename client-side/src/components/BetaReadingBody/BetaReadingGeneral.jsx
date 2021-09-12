81a931897955aeea4d7b8f18b02d9e2e4c97142d
GrakePCH <1152932438@qq.com>
2021-09-12 00:40:39 -0700

    Major BetaReading Subpage Update

--- client-side/src/components/BetaReadingBody/BetaReadingGeneral.jsx
+++ client-side/src/components/BetaReadingBody/BetaReadingGeneral.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 
-import { Container, Grid, Accordion, AccordionSummary, AccordionDetails, Typography } from '@material-ui/core';
+import { Container, Grid, Accordion, AccordionSummary, AccordionDetails, Typography, Divider, Button, Chip, Avatar } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import { BookLib } from '../BookLib';
 import { generalList } from './BetaReadingGeneralList';
+import ArrowForward from '@material-ui/icons/ArrowForward';
 
 
 const useStyles = makeStyles((theme) => ({
   bg: {
     position: 'absolute',
     backgroundColor: '#855cde',
-    height: 320,
+    height: 350,
     width: '100%',
     zIndex: -1,
   },
@@ -33,12 +34,34 @@ const useStyles = makeStyles((theme) => ({
   title: {
     paddingBottom: theme.spacing(1),
     fontWeight: 'bold',
+  },
+  accordion: {
+    // borderRadius: theme.shape.borderRadius,
+  },
+  cover: {
+    height: 180,
+    width: 120,
+    display: 'block',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  bookInfos: {
+    paddingLeft: theme.spacing(4),
+  },
+  authorChips: {
+    margin: "4px 8px 4px 0",
+    "&:last-child":
+      { marginRight: 0, }
+  },
+  buttonGroup: {
+    display: 'flex',
+    flexDirection: 'row-reverse',
+    paddingTop: theme.spacing(1)
   }
 }));
 
 const BetaReadingGeneral = ({ theme }) => {
   const classes = useStyles();
-  console.log("general");
   return (
     <>
       <div className={classes.bg}></div>
@@ -50,18 +73,64 @@ const BetaReadingGeneral = ({ theme }) => {
             <Typography variant='body1'>Works in general advising are looking for feedback on the overall chapter or story. Authors would like to hear the experiences of readers.</Typography>
           </div>
         </Container>
-        {
-          generalList.map((item, index) => (
-            <Accordion>
-              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography>{BookLib[item].title}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>{BookLib[item].info}</Typography>
-              </AccordionDetails>
-            </Accordion>
-          ))
-        }
+        <div>
+          {
+            generalList.map((item, index) => (
+              <Accordion classes={{expanded: classes.accordion}}>
+
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                  <div style={{ backgroundImage: `url(${BookLib[item].cover})` }} className={classes.cover} />
+                  <div className={classes.bookInfos}>
+                    <Typography variant="h5" style={{ fontWeight: "bold" }}>{BookLib[item].title}</Typography>
+                    <Grid>
+                      {
+                        BookLib[item].author.map(
+                          (item, i) =>
+                            <Chip
+                              label={item}
+                              avatar={<Avatar src="" />}
+                              className={classes.authorChips}
+                            />
+                        )
+                      }
+                    </Grid>
+                    <Typography>Genre(s): Educational, Sci-Fi, Thriller</Typography>
+                    <Typography>Status: Ongoing</Typography>
+                    <Typography>Last Updated: April 12, 2021</Typography>
+                  </div>
+                </AccordionSummary>
+
+                <AccordionDetails>
+                  <Container style={{ padding: 0 }}>
+                    <Typography variant="h6">
+                      Synopsis:
+                    </Typography>
+                    <Typography>
+                      {BookLib[item].info}
+                    </Typography>
+                    <Divider variant='middle' style={{ margin: "16px 0" }}></Divider>
+                    <Typography variant="h6" color="primary">
+                      Proceed To Questionnaire?
+                    </Typography>
+                    <Typography>
+                      By answering this questionnaire, you acknowledge and agree to keep this story confidential until its official release.
+                    </Typography>
+                    <div className={classes.buttonGroup}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        endIcon={<ArrowForward />}
+                      >
+                        Agree and continue
+                      </Button>
+                    </div>
+                  </Container>
+                </AccordionDetails>
+
+              </Accordion>
+            ))
+          }
+        </div>
       </Container>
     </>
   )
