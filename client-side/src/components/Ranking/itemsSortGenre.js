e1230c8c1fd3bdc514cad0384fbfe8ba43fdfa33
GrakePCH <1152932438@qq.com>
2021-09-27 01:42:39 -0700

    Format

--- client-side/src/components/Ranking/itemsSortGenre.js
+++ client-side/src/components/Ranking/itemsSortGenre.js
@@ -1,11 +1,4 @@
-import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
-import TrendingUpOutlinedIcon from '@material-ui/icons/TrendingUpOutlined';
-import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
-import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined';
-import UpdateOutlinedIcon from '@material-ui/icons/UpdateOutlined';
-import WhatshotOutlinedIcon from '@material-ui/icons/WhatshotOutlined';
-
-import React, { useState, useEffect, Component  } from 'react';
+import React from 'react';
 import Icon from '@mdi/react';
 import {
   mdiAllInclusive,
@@ -24,58 +17,58 @@ import {
 
 export const itemsSortGenre = [
   {
-  title: 'Overall',
-  icon: <Icon path={mdiAllInclusive} size={1}/>,
-  cName: 'nav-text'
+    title: 'Overall',
+    icon: <Icon path={mdiAllInclusive} size={1} />,
+    cName: 'nav-text'
   },
-    {
+  {
     title: 'Action',
-    icon: <Icon path={mdiRun} size={1}/>,
+    icon: <Icon path={mdiRun} size={1} />,
     cName: 'nav-text'
-    },
-    {
+  },
+  {
     title: 'Fantasy',
-    icon: <Icon path={mdiDeathlyHallows} size={1}/>,
+    icon: <Icon path={mdiDeathlyHallows} size={1} />,
     cName: 'nav-text'
-    },
-    {
+  },
+  {
     title: 'Romance',
-    icon: <Icon path={mdiHeartOutline} size={1}/>,
+    icon: <Icon path={mdiHeartOutline} size={1} />,
     cName: 'nav-text'
-    },
-    {
+  },
+  {
     title: 'Sci-Fi',
-    icon: <Icon path={mdiDeathStar} size={1}/>,
+    icon: <Icon path={mdiDeathStar} size={1} />,
     cName: 'nav-text'
-    },
-    {
+  },
+  {
     title: 'Dystopian',
-    icon: <Icon path={mdiEye} size={1}/>,
+    icon: <Icon path={mdiEye} size={1} />,
     cName: 'nav-text'
-    },
-    {
+  },
+  {
     title: 'Mystery',
-    icon: <Icon path={mdiHelpBox} size={1}/>,
+    icon: <Icon path={mdiHelpBox} size={1} />,
     cName: 'nav-text'
-    },
-    {
+  },
+  {
     title: 'Horror',
-    icon: <Icon path={mdiGhostOutline} size={1}/>,
+    icon: <Icon path={mdiGhostOutline} size={1} />,
     cName: 'nav-text'
-    },
-    {
+  },
+  {
     title: 'Thriller',
-    icon: <Icon path={mdiEmoticonFrown} size={1}/>,
+    icon: <Icon path={mdiEmoticonFrown} size={1} />,
     cName: 'nav-text'
-    },
-    {
+  },
+  {
     title: 'Non-fiction',
-    icon: <Icon path={mdiCassette} size={1}/>,
+    icon: <Icon path={mdiCassette} size={1} />,
     cName: 'nav-text'
-    },
-    {
+  },
+  {
     title: 'Fan-Fiction',
-    icon: <Icon path={mdiAccountGroupOutline} size={1}/>,
+    icon: <Icon path={mdiAccountGroupOutline} size={1} />,
     cName: 'nav-text'
-    },
-  ];
+  },
+];
