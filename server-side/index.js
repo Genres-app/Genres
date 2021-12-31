357082b9ca8a2af125fb888c21b00a3fa2622d30
GrakePCH <1152932438@qq.com>
2021-12-30 19:20:47 -0800

    Update MongoDB URL

--- server-side/index.js
+++ server-side/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use('/user', userRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://xianwew:wxwwxw123@cluster0.mx3za.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://Genres00:Genres00@genresdb.oavqr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
