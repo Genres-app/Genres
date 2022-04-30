6eb0bd940f6ce2756563867b81c1c4f6eee0aaa4
GrakePCH <1152932438@qq.com>
2022-04-30 15:58:22 -0700

    Update publish popup

--- client-side/src/components/WritingPage/AgreeWithCopyright.jsx
+++ client-side/src/components/WritingPage/AgreeWithCopyright.jsx
@@ -12,7 +12,15 @@ import FormControl from '@material-ui/core/FormControl';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
-import { Typography, useTheme } from "@material-ui/core";
+import { Slider, Typography, useTheme } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
+
+const CopyrightSlider = withStyles({
+  root: {
+    // color: '#52af77',
+    height: 8,
+  }
+})(Slider);
 
 const CopyrightDialog = forwardRef((props, ref) => {
 
@@ -48,6 +56,33 @@ const CopyrightDialog = forwardRef((props, ref) => {
   const [activeStep, setActiveStep] = useState(0);
   const steps = ["Select a copyright type", "Read the agreement", "Confirm and publish"];
 
+  const [selectedCopyright, setCopyright] = useState(0);
+  const copyrightsList = [
+    {
+      value: 0,
+      label: "All rights reserved",
+      detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla turpis elit, pulvinar vel tellus ut, congue ultricies elit."
+    },
+    {
+      value: 1,
+      label: "CC1",
+      detail: "Fusce tristique velit dolor, eget interdum risus dictum vitae. Nam sapien quam, vestibulum non viverra a, rhoncus id leo. Quisque feugiat est orci, nec mollis eros convallis ac."
+    },
+    {
+      value: 2,
+      label: "CC2",
+      detail: "Aenean varius enim dolor, at consequat libero consequat id. Sed consequat, felis vel faucibus interdum, urna justo lobortis nulla, quis suscipit diam libero id nulla.",
+    },
+    {
+      value: 3,
+      label: "Open source",
+      detail: "Duis eu purus ut tellus laoreet sollicitudin. Suspendisse id imperdiet lorem, tempor hendrerit tellus. Donec tincidunt mi quis neque vulputate, quis scelerisque metus rhoncus.",
+    }
+  ];
+  const handleCopyrightSelect = (e) => {
+    setCopyright(e.target.value);
+  }
+
   function getStepContent(step) {
     switch (step) {
       case 0:
@@ -56,6 +91,33 @@ const CopyrightDialog = forwardRef((props, ref) => {
             <Typography>
               Please select a copyright type below. <span style={{ color: theme.palette.warning.main }}>Caution! After publishing the book with a copyright license, you can only change it to a looser one.</span>
             </Typography>
+            <div
+              style={{ padding: "0", height: "10rem", paddingTop: "2rem", display: "flex" }}
+            >
+              {/* <CopyrightSlider
+                orientation="vertical"
+                defaultValue={0}
+                step={null}
+                marks={copyrightsList}
+                valueLabelDisplay="off"
+                max={copyrightsList.length - 1}
+                track={false}
+                onChange={handleCopyrightSelect}
+              /> */}
+              <FormControl component="fieldset">
+                <RadioGroup name="copyrightSel" value={selectedCopyright} onChange={handleCopyrightSelect}>
+                  {copyrightsList.map(item => (
+                    <FormControlLabel value={item.value} control={<Radio />} label={item.label} />
+                  ))
+                  }
+                </RadioGroup>
+              </FormControl>
+              <Typography variant="body2" style={{ marginLeft: "1rem", paddingBottom: "1rem" }}>
+                {copyrightsList[selectedCopyright].detail}
+              </Typography>
+            </div>
+
+
             <DialogActions style={{ padding: 0 }}>
               <Button onClick={handleClose} color="primary">
                 Cancel
@@ -71,10 +133,10 @@ const CopyrightDialog = forwardRef((props, ref) => {
       case 1:
         return (
           <>
-            <Typography style={{paddingBottom: "1rem"}}>
+            <Typography style={{ paddingBottom: "1rem" }}>
               Please read and agree with following agreement before publishing the book.
             </Typography>
-            <Typography variant="body2" style={{paddingBottom: "1rem"}}>
+            <Typography variant="body2" style={{ paddingBottom: "1rem" }}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla turpis elit, pulvinar vel tellus ut, congue ultricies elit. Fusce tristique velit dolor, eget interdum risus dictum vitae. Nam sapien quam, vestibulum non viverra a, rhoncus id leo. Quisque feugiat est orci, nec mollis eros convallis ac. Aenean varius enim dolor, at consequat libero consequat id. Sed consequat, felis vel faucibus interdum, urna justo lobortis nulla, quis suscipit diam libero id nulla. Duis eu purus ut tellus laoreet sollicitudin. Suspendisse id imperdiet lorem, tempor hendrerit tellus. Donec tincidunt mi quis neque vulputate, quis scelerisque metus rhoncus.
             </Typography>
             <DialogActions style={{ padding: 0 }}>
@@ -93,13 +155,12 @@ const CopyrightDialog = forwardRef((props, ref) => {
           </>
         );
       case 2:
-        return(
-          
+        return (
           <>
-            <Typography style={{paddingBottom: "1rem"}}>
+            <Typography style={{ paddingBottom: "1rem" }}>
               One step to go. Please confirm you are publishing this book.
             </Typography>
-            <Typography style={{paddingBottom: "1rem"}}>
+            <Typography style={{ paddingBottom: "1rem" }}>
               By clicking the Publish button, you will reveal your works to the public.
             </Typography>
             <DialogActions style={{ padding: 0 }}>
@@ -138,7 +199,7 @@ const CopyrightDialog = forwardRef((props, ref) => {
             padding: "1.5rem",
             paddingBottom: 0
           }}>
-          Publish Book
+          Publish
         </Typography>
         <Stepper activeStep={activeStep} style={{ backgroundColor: "transparent" }}>
           {steps.map((label, index) => {
